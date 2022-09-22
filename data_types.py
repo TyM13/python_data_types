9e323b869901772bc3dbc8d4e9cbc5f040d4b153
Your Name <you@example.com>
2022-09-22 17:43:39 -0600

    large/small num

--- data_types.py
+++ data_types.py
@@ -1,7 +1,16 @@
 string_one = 'i am the number one sting'
-string_two = 'string number two test'
+string_two = 'string number two'
 
 if(string_one.endswith('test') or string_two.endswith('test')):
     print('found test')
 else:
     print('no test')
+
+
+    list_of_numbers = [2,5,9,17]
+
+    for number in list_of_numbers:
+        if(number > 10):
+            print('large', number)
+        elif(number < 10):
+            print('not large', number)
