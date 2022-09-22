ba1a2b02b6d8462086728878831589b09579c091
Your Name <you@example.com>
2022-09-22 17:23:49 -0600

    string

---
+++ data_types.py
@@ -0,0 +1,7 @@
+string_one = 'i am the number one sting'
+string_two = 'string number two test'
+
+if(string_one.endswith('test') or string_two.endswith('test')):
+    print('found test')
+else:
+    print('no test')
