4f9bd7c10070e1369b9e4b2e83d799113d32b8d1
Your Name <you@example.com>
2022-09-22 18:43:15 -0600

    dictonaries and looping

--- data_types.py
+++ data_types.py
@@ -1,14 +1,38 @@
-string_one = 'i am the number one sting'
-string_two = 'string number two'
-list_of_numbers = [2,5,9,17]
+# string_one = 'i am the number one sting'
+# string_two = 'string number two'
+# list_of_numbers = [2,5,9,17]
 
-if(string_one.endswith('test') or string_two.endswith('test')):
-    print('found test')
-else:
-    print('no test')
+# if(string_one.endswith('test') or string_two.endswith('test')):
+#     print('found test')
+# else:
+#     print('no test')
 
-for number in list_of_numbers:
-    if(number > 10):
-        print('large', number)
-    elif(number < 10):
-        print('not large', number)
+# for number in list_of_numbers:
+#     if(number > 10):
+#         print('large', number)
+#     elif(number <= 10):
+#         print('not large', number)
+
+
+clients =  [
+{
+        'username': 'user_one',
+        'age': 12,
+        'friends': ['friend_one','friend_two','friend_three']
+    },
+    {
+        'username': 'user_two',
+        'age': 18,
+        'friends': ['sam','ty']
+    },
+    {
+        'username': 'user_three',
+        'age': 21,
+        'friends': ['ty']
+    }
+]
+
+for client in clients:
+    print(client['username'], client['age'])
+    for friend in client['friends']:
+        print(friend)
